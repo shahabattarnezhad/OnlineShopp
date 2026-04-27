3962fb81b6ec95bb7a6c04c71fe939b7e1bbce33
Shahab Attarnejad <atarnezhad@hotmail.com>
2026-04-27 04:56:10 -0700

    The TypeRepository is implemented.

---
+++ Services/Catalog/Catalog/Repositories/Implementations/TypeRepository.cs
@@ -0,0 +1,32 @@
+﻿using Catalog.Entities;
+using Catalog.Repositories.Interfaces;
+using MongoDB.Driver;
+
+namespace Catalog.Repositories.Implementations;
+
+public class TypeRepository : ITypeRepository
+{
+    private readonly IMongoCollection<ProductType> _types;
+
+    public TypeRepository(IConfiguration configuration)
+    {
+        var client =
+            new MongoClient(configuration["DatabaseSettings:ConnectionString"]);
+
+        var db =
+            client.GetDatabase(configuration["DatabaseSettings:DatabaseName"]);
+
+        _types =
+            db.GetCollection<ProductType>(configuration["DatabaseSettings:TypeCollectionName"]);
+    }
+
+    public async Task<IEnumerable<ProductType>> GetAllTypesAsync()
+    {
+        return await _types.Find(_ => true).ToListAsync();
+    }
+
+    public async Task<ProductType> GetTypeByIdAsync(string typeId)
+    {
+        return await _types.Find(t => t.Id == typeId).FirstOrDefaultAsync();
+    }
+}
