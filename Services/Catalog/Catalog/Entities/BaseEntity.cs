1d2d2470077cb81836cac673046a4628f50945ca
Shahab Attarnejad <atarnezhad@hotmail.com>
2026-04-26 23:22:36 -0700

    The BaseEntity is created in the Catalog service.

---
+++ Services/Catalog/Catalog/Entities/BaseEntity.cs
@@ -0,0 +1,11 @@
+﻿using MongoDB.Bson.Serialization.Attributes;
+using MongoDB.Bson;
+
+namespace Catalog.Entities;
+
+public class BaseEntity
+{
+    [BsonId]
+    [BsonRepresentation(BsonType.ObjectId)]
+    public string Id { get; set; }
+}
