ad6c7d8a7c4ae1fb944ebbbe445056f19f96f7e4
Shahab Attarnejad <atarnezhad@hotmail.com>
2026-04-27 07:00:53 -0700

    The DatabaseSeeder is defined.

---
+++ Services/Catalog/Catalog/Data/DatabaseSeeder.cs
@@ -0,0 +1,79 @@
+﻿using Catalog.Entities;
+using MongoDB.Driver;
+using System;
+using System.Text.Json;
+
+namespace Catalog.Data;
+
+public class DatabaseSeeder
+{
+    public static async Task SeedAsync(IConfiguration configuration)
+    {
+        var client =
+            new MongoClient(configuration["DatabaseSettings:ConnectionString"]);
+
+        var db =
+            client.GetDatabase(configuration["DatabaseSettings:DatabaseName"]);
+
+        var products =
+            db.GetCollection<Product>(configuration["DatabaseSettings:ProductCollectionName"]);
+        var brands =
+            db.GetCollection<ProductBrand>(configuration["DatabaseSettings:BrandCollectionName"]);
+        var types =
+            db.GetCollection<ProductType>(configuration["DatabaseSettings:TypeCollectionName"]);
+
+        var SeedBasePath = Path.Combine("Data", "SeedData");
+
+
+        // Seed Brands
+        List<ProductBrand> brandList = new();
+        if ((await brands.CountDocumentsAsync(_ => true)) == 0)
+        {
+            var brandData =
+                await File.ReadAllTextAsync(Path.Combine(SeedBasePath, "brands.json"));
+
+            brandList = JsonSerializer.Deserialize<List<ProductBrand>>(brandData);
+
+            await brands.InsertManyAsync(brandList);
+        }
+        else
+        {
+            brandList = await brands.Find(_ => true).ToListAsync();
+        }
+
+        // Seed Types
+        List<ProductType> typeList = new();
+        if ((await types.CountDocumentsAsync(_ => true)) == 0)
+        {
+            var typeData =
+                await File.ReadAllTextAsync(Path.Combine(SeedBasePath, "types.json"));
+
+            typeList = JsonSerializer.Deserialize<List<ProductType>>(typeData);
+
+            await types.InsertManyAsync(typeList);
+        }
+        else
+        {
+            typeList = await types.Find(_ => true).ToListAsync();
+        }
+
+        // Seed Products
+        if((await products.CountDocumentsAsync(_ => true )) == 0)
+        {
+            var productData = 
+                await File.ReadAllTextAsync(Path.Combine(SeedBasePath, "products.json"));
+
+            var productList = JsonSerializer.Deserialize<List<Product>>(productData);
+
+            foreach (var product in productList)
+            {
+                product.Id = null;
+
+                if(product.CreatedDate == default)
+                    product.CreatedDate = DateTime.UtcNow;
+            }
+
+            await products.InsertManyAsync(productList);
+        }
+    }
+}
