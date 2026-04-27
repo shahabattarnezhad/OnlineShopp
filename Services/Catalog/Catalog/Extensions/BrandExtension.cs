775d38b0081ec96badd1e7748b365b200c16f201
Shahab Attarnejad <atarnezhad@hotmail.com>
2026-04-27 08:06:18 -0700

    The GetAllBrandsHandler and also the BrandExtension are created.

---
+++ Services/Catalog/Catalog/Extensions/BrandExtension.cs
@@ -0,0 +1,21 @@
+﻿using Catalog.Entities;
+using Catalog.Responses.Brand;
+
+namespace Catalog.Extensions;
+
+public static class BrandExtension
+{
+    public static BrandResponse ToResponse(this ProductBrand brand)
+    {
+        return new BrandResponse
+        {
+            Id = brand.Id,
+            Name = brand.Name
+        };
+    }
+
+    public static IList<BrandResponse> ToResponseList(this IEnumerable<ProductBrand> brands)
+    {
+        return brands.Select(brand => brand.ToResponse()).ToList();
+    }
+}
---
+++ Services/Catalog/Catalog/Handlers/Brand/GetAllBrandsHandler.cs
@@ -0,0 +1,18 @@
+﻿using Catalog.Extensions;
+using Catalog.Queries.Brand;
+using Catalog.Repositories.Interfaces;
+using Catalog.Responses.Brand;
+using MediatR;
+
+namespace Catalog.Handlers.Brand;
+
+public class GetAllBrandsHandler(IBrandRepository brandRepository)
+                                : IRequestHandler<GetAllBrandsQuery, IList<BrandResponse>>
+{
+    public async Task<IList<BrandResponse>> Handle(GetAllBrandsQuery request, CancellationToken cancellationToken)
+    {
+        var brandList = await brandRepository.GetAllBrandsAsync();
+
+        return brandList.ToResponseList();
+    }
+}
