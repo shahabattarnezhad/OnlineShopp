048dd2d2574ab1e29c8de9eb27d2933453eece8e
Shahab Attarnejad <atarnezhad@hotmail.com>
2026-04-28 23:12:52 -0700

    The GetProductByIdHandler and the GetProductByIdQuery are created.

---
+++ Services/Catalog/Catalog/Handlers/Product/GetProductByIdHandler.cs
@@ -0,0 +1,19 @@
+﻿using Catalog.Mappers;
+using Catalog.Queries.Product;
+using Catalog.Repositories.Interfaces;
+using Catalog.Responses.Product;
+using MediatR;
+
+namespace Catalog.Handlers.Product;
+
+public record GetProductByIdHandler(IProductRepository ProductRepository) 
+              : IRequestHandler<GetProductByIdQuery, ProductResponse>
+{
+    public async Task<ProductResponse> Handle(GetProductByIdQuery request, CancellationToken cancellationToken)
+    {
+        var product = await ProductRepository.GetProductByIdAsync(request.Id);
+        var productResponse = product.ToResponse();
+
+        return productResponse;
+    }
+}
---
+++ Services/Catalog/Catalog/Queries/Product/GetProductByIdQuery.cs
@@ -0,0 +1,8 @@
+﻿using Catalog.Responses.Product;
+using MediatR;
+
+namespace Catalog.Queries.Product;
+
+public record GetProductByIdQuery(string Id) : IRequest<ProductResponse>
+{
+}
