82158d74f113f8c897cac32af643881cf7b7602c
Shahab Attarnejad <atarnezhad@hotmail.com>
2026-04-29 00:04:03 -0700

    The GetAllProductsByNameHandler and the GetAllProductsByNameQuery are created.

---
+++ Services/Catalog/Catalog/Handlers/Product/GetAllProductsByNameHandler.cs
@@ -0,0 +1,21 @@
+﻿using Catalog.Mappers;
+using Catalog.Queries.Product;
+using Catalog.Repositories.Interfaces;
+using Catalog.Responses.Product;
+using MediatR;
+
+namespace Catalog.Handlers.Product;
+
+public record GetAllProductsByNameHandler(IProductRepository productRepository)
+              : IRequestHandler<GetAllProductsByNameQuery, IList<ProductResponse>>
+{
+    public async Task<IList<ProductResponse>> Handle(GetAllProductsByNameQuery request, CancellationToken cancellationToken)
+    {
+        var productList =
+            await productRepository.GetAllProductsByNameAsync(request.Name);
+
+        var productResponseList = productList.ToResponseList().ToList();
+
+        return productResponseList;
+    }
+}
---
+++ Services/Catalog/Catalog/Queries/Product/GetAllProductsByNameQuery.cs
@@ -0,0 +1,8 @@
+﻿using Catalog.Responses.Product;
+using MediatR;
+
+namespace Catalog.Queries.Product;
+
+public record GetAllProductsByNameQuery(string Name) : IRequest<IList<ProductResponse>>
+{
+}
